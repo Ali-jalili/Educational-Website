d66b9356d9106fe4fb4e756fd3a2a6d9983e3732
Fatemeh <fatemehzalaghi@gmail.com>
2023-08-11 19:35:12 +0330

    About-Home and wrapper-home sections have been completed

--- my-educational/src/Components/Page/Home/Main/Main.jsx
+++ my-educational/src/Components/Page/Home/Main/Main.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Main.css'
 
 const Main = () => {
     return (
@@ -9,12 +10,155 @@ const Main = () => {
 
                 <div className='about-home-section container'>
 
-                    <div className='imag-about'>
-                        <img src="../../../../../public/Img/linkedin-sales-solutions-0QvTyp0gH3A-unsplash.jpg" alt="" />sfhdfhd
+                    <h2 className='title-about-home'>About Us</h2>
+
+
+                    <div className="box-info-about">
+
+                        <div className='imag-about'>
+
+                            <img src="Img/About-img-home.jpg" alt="About-img-home" />
+
+                        </div>
+
+
+                        <div className="text-info-about">
+
+                            <div className='heading-info-about'>
+                                <h3 className='title-about-1'>LEARN ANYTHING</h3>
+                                <h2 className='title-about-2'>Benefits About Online Learning Expertise</h2>
+                            </div>
+
+                            <div className='contact-box'>
+
+                                <div className='my-box'>
+
+                                    <div className='img'>
+                                        <img src="img/storytelling.png" alt="" />
+                                    </div>
+
+                                    <div>
+
+                                        <h3 className='box-title'>Online Courses</h3>
+                                        <p className='p-box'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                    </div>
+
+
+                                </div>
+
+                                <div className='my-box'>
+
+                                    <div className='img'>
+                                        <img src="img/diploma.png" alt="" />
+                                    </div>
+
+                                    <div>
+
+                                        <h3 className='box-title'>Earn A Certificates</h3>
+                                        <p className='p-box'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                    </div>
+
+                                </div>
+
+                                <div className='my-box'>
+
+                                    <div className='img'>
+                                        <img src="img/athlete.png" alt="" />
+                                    </div>
+
+                                    <div>
+
+                                        <h3 className='box-title'>
+                                            Learn with Expert</h3>
+                                        <p className='p-box'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                    </div>
+
+                                </div>
+
+
+
+                            </div>
+
+
+
+
+
+                        </div>
+
                     </div>
 
                 </div>
 
+
+
+            </section>
+
+            <section className='awrapper-home'>
+
+                <div className='awrapper container'>
+
+                    <div className='sub-data'>
+
+                        <div className='data-box'>
+
+                            <div className='data-box-icon'>
+                                <i class="fa-solid fa-user-graduate"></i>
+                            </div>
+
+                            <div className='data-box-text'>
+                                <span>3,000</span>
+                                <span>SUCCESS STORIES</span>
+                            </div>
+                        </div>
+
+                        <div className='data-box'>
+
+                            <div className='data-box-icon'>
+                                <i class="fa-solid fa-user-graduate"></i>
+                            </div>
+
+                            <div className='data-box-text'>
+                                <span>320</span>
+                                <span>TRUSTED TUTORS</span>
+                            </div>
+                        </div>
+
+                        <div className='data-box'>
+
+                            <div className='data-box-icon'>
+                                <i class="fa-solid fa-user-graduate"></i>
+                            </div>
+
+                            <div className='data-box-text'>
+                                <span>1,000</span>
+                                <span>SCHEDULES</span>
+                            </div>
+                        </div>
+
+                        <div className='data-box'>
+
+                            <div className='data-box-icon'>
+                                <i class="fa-solid fa-user-graduate"></i>
+                            </div>
+
+                            <div className='data-box-text'>
+                                <span>120</span>
+                                <span>COURSES</span>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+                    </div>
+
+                </div>
 
 
             </section>
